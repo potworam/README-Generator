40c7c47f65eadd7170cd6316b8e2502901519040
Potwora <matthewpotwora@yahoo.com>
2022-07-02 12:04:19 -0700

    added HUGE features

--- idex.js
+++ idex.js
@@ -1,16 +1,44 @@
 
 // TODO: Include packages needed for this application
-import inquirer from 'inquirer';
+const inquirer = require('inquirer');
 const fs = require('fs');
-const generatePage = import('./src/page-template');
+const generatePage = require('./src/page-template.js');
 // TODO: Create an array of questions for user input
-const questions = [];
+  const questions=[
+    {
+        type:'input',
+        name: 'project_name',
+        message: 'What is your projects name? (Required)',
+    },
+    {
+        type:'input',
+        name: 'description',
+        message: 'What is your description? (Required)',
+    }
+    
 
+  ]
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+   let str=generatePage.createProjectTitle(data.project_name)+"\n"+ generatePage.description(data.description)
+   fs.writeFile(fileName, str, err => {
+      if (err) throw new Error(err);
+      console.log('Page created! Check out index.html in this directory to see it!');
+    });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+ async function init(questions) {
+     let answers = await inquirer.prompt(questions)
+     console.log(answers)
+     await writeToFile("./README.md",answers)
+    return answers
+
+}
 
 // Function call to initialize app
-init();
+
+async function app() {
+    await init(questions);
+}
+app();
---
+++ src/page-template.js
@@ -0,0 +1,8 @@
+
+ const createProjectTitle = function(funyons){ 
+    return `# ${funyons}`
+}
+ const description=(cheetos)=>{
+    return `# ${cheetos}`
+}
+module.exports={createProjectTitle,description}
